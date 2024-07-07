dbef45acdc08e86f402610504f1011031c778277
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-07-07 18:06:17 -0500

    parking fixes

---
+++ Assets/Scripts/LevelParking/WinZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinZone : MonoBehaviour {
+
+
+	public GameObject winText;
+
+
+	void Start () {
+		
+		winText.gameObject.SetActive (false);
+	}
+
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+	    if (other.CompareTag("MainCar"))
+        {
+            winText.SetActive(true);
+        }
+	}
+	
+	
+}
