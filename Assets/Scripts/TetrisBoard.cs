fa6f06a8f01fe43d55104dd66dacf8309669d63a
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-07-06 23:42:21 -0500

    parking almost working

--- Assets/Scripts/TetrisBoard.cs
+++ Assets/Scripts/TetrisBoard.cs
@@ -1,177 +1,204 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class TetrisBoard : MonoBehaviour
 {
-    [SerializeField] 
-    private GameObject blockPrefab;
-    [SerializeField] 
-    private Sprite[] blockSprite;
 
-    private struct Block {
+    [SerializeField]
+    public GameObject blockPrefab;
+
+    [SerializeField]
+    public Sprite[] blockSprite;
+
+    public struct Block
+    {
         public int x;
         public int y;
         public GameObject ob;
 
-        public Block(int x, int y, GameObject ob) {
+        public Block(int x, int y, GameObject ob)
+        {
             this.x = x;
             this.y = y;
             this.ob = ob;
         }
     }
 
-    private Block[] piece = new Block[4] {
+    public Block[] piece = new Block[4]
+    {
         new Block(),
         new Block(),
-        new Block(),        
         new Block(),
+        new Block()
     };
 
-    private int W = 10;
-    private int H = 20;
-    private Block[,] block;
-    private int[,] shapes = new int[,]{
-        {1,3,5,7}, // L
-        {2,4,5,7}, // Z
-        {3,4,5,6}, // S
-        {3,4,5,7}, // T
-        {2,3,5,7}, // L
-        {3,5,6,7}, // J
-        {2,3,4,5}, // O
-    };
+    public int W = 10;
+
+    public int H = 20;
+
+    public Block[,] block;
+
+    public int[,] shapes = new int[,]
+    {
+        {1,3,5,7}, //l
+		{2,4,5,7}, //Z
+		{3,4,5,6}, //S
+		{3,4,5,7}, //T
+		{2,3,5,7}, //L
+		{3,5,6,7}, //J
+		{2,3,4,5}, //O
+	};
+
+    public float moveTime = 0;
     public float moveSpeed = 0.06f;
+    public float time = 0;
     public float dropSpeed = 0.4f;
-    private float moveTime = 0;
-    private float time = 0;
-    void Start() {
-        block = new Block[W,H];
+
+    void Start()
+    {
+        block = new Block[W, H];
         Generate();
     }
 
-    void Update() {
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            HoldAndMove(-1, 0);
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            HoldAndMove(1, 0);
-        } else if (Input.GetKey(KeyCode.UpArrow)) {
-            Rotate(); // rotate  
-        } else if (Input.GetKey(KeyCode.DownArrow)) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Hold(-1, 0);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            Hold(1, 0);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            Rotate();   //rotate
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
             dropSpeed = 0.05f;
-        }
 
         time += Time.deltaTime;
-        if (time > dropSpeed) {
-            // Move(0, -1);
-            if (!Move(0, -1)) {
-                for (int i = 0; i < 4; i++) {
+        if (time > dropSpeed)
+        {
+            if (!Move(0, -1))
+            {
+                for (int i = 0; i < 4; i++)
                     block[piece[i].x, -piece[i].y] = piece[i];
-                }
                 Generate();
-                Clear();
+				Clear();
             }
             time = 0;
-        }      
+        }
+
     }
 
-    private void Generate() {
-        dropSpeed = 0.05f;
+    public void Generate()
+    {
+        dropSpeed = 0.4f;
         int n = Random.Range(0, shapes.GetLength(0));
-        for (int i = 0; i < 4; i++) {
-           piece[i].x = shapes[n, i] % 2; 
-           piece[i].y = -shapes[n, i] / 2; 
+        for (int i = 0; i < 4; i++)
+        {
+            piece[i].x = shapes[n, i] % 2;
+            piece[i].y = -shapes[n, i] / 2;
         }
+
         Sprite sprite = blockSprite[Random.Range(0, blockSprite.Length)];
-        for (int i = 0; i < 4; i++) {
-           piece[i].ob = Instantiate(blockPrefab, new Vector2(piece[i].x, piece[i].y), Quaternion.identity); 
+        for (int i = 0; i < 4; i++)
+        {
+            piece[i].ob = Instantiate(blockPrefab, new Vector2(piece[i].x, piece[i].y), Quaternion.identity);
             SpriteRenderer sr = piece[i].ob.GetComponent<SpriteRenderer>();
             sr.sprite = sprite;
         }
-        // Move(4, 0);
+		Move(4,0);
     }
 
-    private void HoldAndMove(int dx, int dy) {
+
+
+    public void Hold(int dx, int dy)
+    {
         moveTime += Time.deltaTime;
-        if (moveTime > moveSpeed) {
+        if (moveTime > moveSpeed)
+        {
             Move(dx, dy);
             moveTime = 0;
         }
     }
 
-    private bool Move(int dx, int dy) {
+    public bool Move(int dx, int dy)
+    {
         Block[] origin = piece.Clone() as Block[];
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++)
+        {
             piece[i].x += dx;
             piece[i].y += dy;
-                // piece[i].ob.transform.position = new Vector2(piece[i].x, piece[i].y);
         }
+
         return CheckAndSet(origin);
     }
 
-    private void Rotate() {
+    public void Rotate()
+    {
         Block[] origin = piece.Clone() as Block[];
-        Block p = piece[1];
-        for (int i = 0; i < 4; i++) {
+        Block p = piece[1]; //center of rotation
+        for (int i = 0; i < 4; i++)
+        {
             int x = piece[i].y - p.y;
             int y = piece[i].x - p.x;
             piece[i].x = p.x - x;
             piece[i].y = p.y + y;
-                piece[i].ob.transform.position = new Vector2(piece[i].x, piece[i].y);
-
         }
         CheckAndSet(origin);
+
     }
 
-    private bool CheckAndSet(Block[] ori) {
+    private bool CheckAndSet(Block[] ori)
+    {
         bool set = true;
-        for (int i = 0; i < 4; i++) {
-            if (piece[i].x < 0 || piece[i].x >= W || piece[i].y <= -H) // out of bound
-            {
+        for (int i = 0; i < 4; i++)
+        {
+            if (piece[i].x < 0 || piece[i].x >= W || piece[i].y <= -H) 
                 set = false;
-            } else if (block[piece[i].x, -piece[i].y].ob != null) // space occupied 
-            {
+            else if (block[piece[i].x, -piece[i].y].ob != null) 
                 set = false;
-            }
         }
-        if (set) {
-            for (int i = 0; i < 4; i++) {
+        if (set)
+            for (int i = 0; i < 4; i++)
                 piece[i].ob.transform.position = new Vector2(piece[i].x, piece[i].y);
-            }
-        } else {
+        else
             piece = ori;
-        }
         return set;
     }
 
-    private void Clear() {
+    public void Clear()
+    {
         List<Block> blockToClear = new List<Block>();
         int k = H - 1;
         int dy = 0;
-        for (int i = H - 1; i > 0; i--) {
+        for (int i = H - 1; i > 0; i--)
+        {
             blockToClear.Clear();
             int count = 0;
-            for (int j = 0; j < W; j++) {
-                if (block[j, i].ob != null) {
+            for (int j = 0; j < W; j++)
+            {
+                if (block[j, i].ob != null)
                     count++;
-                }
+
                 block[j, i].y += dy;
                 blockToClear.Add(block[j, i]);
                 block[j, k] = block[j, i];
-            }   
-            if (count < W) {
+            }
+            if (count < W)
                 k--;
-            } else {
+            else
+            {
                 dy += -1;
-                for (int n = 0; n < blockToClear.Count; n++) {
+                for (int n = 0; n < blockToClear.Count; n++)
                     Destroy(blockToClear[n].ob);
-                }
             }
-            for (int j = 0; j < W; j++) {
-                if (block[j, i].ob != null) {
+
+            for (int j = 0; j < W; j++) //
+            {
+                if (block[j, i].ob != null)
                     block[j, i].ob.transform.position = new Vector2(block[j, i].x, block[j, i].y);
-                }
             }
-        }        
+        }
     }
 }
-
---
+++ Assets/Scripts/TouchDrag.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private Collider2D objectCollider;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        objectCollider = GetComponent<Collider2D>();
+
+        if (objectCollider == null)
+        {
+            Debug.LogError("Collider2D is missing on " + gameObject.name);
+        }
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f; // Reduce opacity for visual feedback
+        canvasGroup.blocksRaycasts = false; // Allows to drop over other UI elements
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (objectCollider == null) return; // Skip drag handling if Collider2D is missing
+
+        Vector2 newPosition = rectTransform.anchoredPosition + eventData.delta / canvas.scaleFactor;
+
+        // Check if the new position would collide with an obstacle
+        if (!IsCollidingWithObstacle(newPosition))
+        {
+            rectTransform.anchoredPosition = newPosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1.0f; // Reset opacity
+        canvasGroup.blocksRaycasts = true; // Restore raycast blocking
+    }
+
+    private bool IsCollidingWithObstacle(Vector2 newPosition)
+    {
+        if (objectCollider == null) return false; // Skip collision check if Collider2D is missing
+
+        Vector3[] worldCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(worldCorners);
+
+        Vector3 shift = new Vector3(newPosition.x - rectTransform.anchoredPosition.x, newPosition.y - rectTransform.anchoredPosition.y, 0);
+        for (int i = 0; i < worldCorners.Length; i++)
+        {
+            worldCorners[i] += shift;
+        }
+
+        Bounds newBounds = new Bounds(worldCorners[0], Vector3.zero);
+        newBounds.Encapsulate(worldCorners[1]);
+        newBounds.Encapsulate(worldCorners[2]);
+        newBounds.Encapsulate(worldCorners[3]);
+
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(newBounds.center, newBounds.size, 0);
+
+        foreach (Collider2D collider in hitColliders)
+        {
+            if (collider != objectCollider && collider.CompareTag("Obstaculo"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
