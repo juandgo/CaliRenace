8c31312d18c9175acba7cd454b549233c55ea620
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-07-08 18:39:51 -0500

    .. progress show modal

--- Assets/Scripts/Levels/GameUI1.cs
+++ Assets/Scripts/Levels/GameUI1.cs
@@ -10,47 +10,45 @@ namespace LevelUnlockSystem
     public class GameUI1 : MonoBehaviour
     {
         [SerializeField] private Image[] starsArray;            //array of stars
-        [SerializeField] public int starCount;            //array of stars
+        [SerializeField] public int starCount;                  //number of stars achieved
         [SerializeField] private Text levelStatusText;          //level status text
         [SerializeField] private GameObject overPanel;          //ref to over panel
         [SerializeField] private Color lockColor, unlockColor;  //ref to colors
 
-    void OnTriggerEnter2D (Collider2D other)
-    {
-	    if (other.CompareTag("MainCar"))
+        void OnTriggerEnter2D(Collider2D other)
         {
-            if (this.starCount > 0)                                  //if start count is more than 0
-            {                                                   //set the levelStatusText
-                levelStatusText.text = "Level " + (LevelSystemManager.Instance.CurrentLevel + 1) + " Complete";
-                LevelSystemManager.Instance.LevelComplete(this.starCount);   //send the information to LevelSystemManager
-            }
-            else
+            if (other.CompareTag("MainCar"))
             {
-                                                                //else only set the levelStatusText
-                levelStatusText.text = "Level " + (LevelSystemManager.Instance.CurrentLevel + 1) + " Failed";
+                if (starCount > 0)                               //if star count is more than 0
+                {
+                    levelStatusText.text = "Level " + (LevelSystemManager.Instance.CurrentLevel + 1) + " Complete";
+                    LevelSystemManager.Instance.LevelComplete(starCount);   //send the information to LevelSystemManager
+                }
+                else                                             //else only set the levelStatusText
+                {
+                    levelStatusText.text = "Level " + (LevelSystemManager.Instance.CurrentLevel + 1) + " Failed";
+                }
+                SetStar(starCount);                              //set the stars based on starCount
+                overPanel.SetActive(true);                       //activate the over panel
             }
-            SetStar(this.starCount);                                 //set the stars
-            overPanel.SetActive(true);                
         }
-	}
-        public void OkBtn()                                     //method called by ok button
+
+        public void OkBtn()                                      //method called by ok button
         {
             SceneManager.LoadScene(0);
         }
 
-
         /// <summary>
-        /// Method to show number of stars achieved by the player for this perticular level
+        /// Method to show number of stars achieved by the player for this particular level
         /// </summary>
         /// <param name="starAchieved"></param>
         private void SetStar(int starAchieved)
         {
-            starAchieved = 3;
             for (int i = 0; i < starsArray.Length; i++)             //loop through entire star array
             {
                 /// <summary>
                 /// if i is less than starAchieved
-                /// Eg: if 2 stars are achieved we set the start at index 0 and 1 color to unlockColor, as array start from 0 element
+                /// Eg: if 2 stars are achieved we set the star at index 0 and 1 color to unlockColor, as array start from 0 element
                 /// </summary>
                 if (i < starAchieved)
                 {
