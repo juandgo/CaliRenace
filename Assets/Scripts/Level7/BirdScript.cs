d55dac83e6152ca8c3c66bdaf4f173f7b9db0175
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-08-21 03:26:41 -0500

    flappybird

--- Assets/Scripts/Level7/BirdScript.cs
+++ Assets/Scripts/Level7/BirdScript.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+// using UnityEngine.Audio;
+using LevelUnlock;  // Asegúrate de incluir el namespace de SaveLoadData
 
 public class BirdScript : MonoBehaviour
 {
@@ -8,10 +11,17 @@ public class BirdScript : MonoBehaviour
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    private int userId;  // ID del usuario
 
     // Start is called before the first frame update
     void Start()
     {
+                // Obtén el userId de PlayerPrefs
+        userId = PlayerPrefs.GetInt("accountUserId", -1);
+        if (userId == -1)
+        {
+            Debug.LogError("No se encontró el ID de usuario guardado.");
+        }
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
@@ -29,4 +39,27 @@ public class BirdScript : MonoBehaviour
         logic.gameOver();
         birdIsAlive = false;
     }
+    public void MainMenu(string level)
+    {
+        // Guarda el nivel completado antes de cambiar de escena
+        if (SaveLoadData.Instance != null)
+        {
+            SaveLoadData.Instance.SaveData(userId, 7, "1", 3);
+        } else {
+            Debug.Log("failure");
+
+        }
+        SceneManager.LoadScene("Levels");
+
+            // panel.SetActive(true);
+            // Time.timeScale = 0f;
+    }
+        // Cambia la escena al menú principal
+        // SceneManager.LoadScene("Levels");
+    public void OkBtn()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Levels");
+    }
+
 }
