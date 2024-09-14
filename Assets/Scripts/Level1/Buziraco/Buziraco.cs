7c14eae40a8675026172e1cd11d141d47b292d0c
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2024-09-14 17:42:22 -0500

    giving life to buziraco

--- Assets/Scripts/Level1/Buziraco/Buziraco.cs
+++ Assets/Scripts/Level1/Buziraco/Buziraco.cs
@@ -9,11 +9,15 @@ public class Buziraco : MonoBehaviour
     public float speed = 2.0f;
     private Rigidbody2D rb; // Cambiado a Rigidbody2D
     private Vector2 movement;
+    private Animator animator;
+
+    private bool onTheMove;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Obtener Rigidbody2D
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,14 +29,23 @@ public class Buziraco : MonoBehaviour
         {
             // Normalizar la dirección del movimiento
             Vector2 direction = (player.position- transform.position).normalized;
+            if(direction.x < 0){
+                transform.localScale = new Vector3(-1, 1, 1);
+            }else{
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
             movement = new Vector2(direction.x, 0); // Usar tanto X como Y si es necesario
+            onTheMove = true;
         }
         else
         {
             movement = Vector2.zero; // Corregido el uso de Vector2.zero
+            onTheMove = false;
         }
 
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime); // Mover con Rigidbody2D
+        animator.SetBool("onTheMove", onTheMove);
     }
 
     void OnDrawGizmosSelected(){
